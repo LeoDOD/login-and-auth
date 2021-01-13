7d0bac70903713c8910b7429347fc34265f979c2
Leo De Oliveira <leo.deoliveira@paya.com>
2021-01-12 21:50:37 -0500

    Commented JS file.

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,3 +1,4 @@
+//Constants
 const phoneFormat = (input) => {
     if (input && !isNaN(input)) {
       if (input.length === 10) document.getElementsByName("phoneNumber")[0].value = input.replace(/(\d{3})(\d{3})(\d{4})/, "$1-$2-$3");
